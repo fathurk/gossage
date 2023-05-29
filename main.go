b5030cf8896c0dcbbc4f284f29623d11cc9523bb
fathurkaryim <fathurkaryim@gmail.com>
2023-05-30 04:22:38 +0700

    feat: add connection to mongodb

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"gossage/handlers"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -19,6 +25,8 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
+	ConnectDb()
+
 	api := router.Group("/api")
 	{
 		v1 := api.Group("/v1")
@@ -30,4 +38,23 @@ func main() {
 		}
 	}
 	s.ListenAndServe()
+}
+
+func ConnectDb() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
 }
