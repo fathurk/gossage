74ca8724c53049f6c37721392dee8ee23740cbf7
fathurkaryim <fathurkaryim@gmail.com>
2023-06-03 12:01:10 +0700

    feat: create create user handler and model

--- handlers/user.go
+++ handlers/user.go
@@ -1,1 +1,73 @@
 package handlers
+
+import (
+	"context"
+	"fmt"
+	"gossage/models"
+	"gossage/utils"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type CreateUserRequestBody struct {
+	UserName    string `json:"user_name" binding:"required"`
+	Email       string `json:"email" binding:"required"`
+	PhoneNumber string `json:"phone_number" binding:"required,lte=15"`
+	Password    string `json:"password" binding:"required"`
+	Bio         string `json:"bio" `
+}
+
+var (
+	DefaultUserSettings = &models.UserSetting{OnlineStatus: "PUBLIC", ProfilePictureSeen: "PUBLIC", ChatByOther: "TRUE"}
+)
+
+func CreateUser(c *gin.Context) {
+
+	var requestBody CreateUserRequestBody
+
+	if err := c.BindJSON(&requestBody); err != nil {
+		Responser(c, 400, err.Error())
+		return
+	}
+
+	client, err := utils.ConnectDb()
+	defer utils.DisconnectDb(client)
+
+	if err != nil {
+		Responser(c, 500, err.Error())
+		return
+	}
+
+	hashed, err := utils.HashPassword(requestBody.Password)
+
+	if err != nil {
+		Responser(c, 500, err.Error())
+	}
+
+	now := time.Now()
+
+	userPayload := &models.User{
+		ID:          primitive.NewObjectID(),
+		CreatedAt:   now,
+		LastOnline:  now,
+		UserName:    requestBody.UserName,
+		Email:       requestBody.Email,
+		PhoneNumber: requestBody.PhoneNumber,
+		Password:    hashed,
+		Bio:         requestBody.Bio,
+		UserSetting: *DefaultUserSettings,
+	}
+
+	result, err := client.Database("gossage").Collection("user").InsertOne(context.TODO(), userPayload)
+
+	fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
+
+	if err != nil {
+		Responser(c, 500, err.Error())
+		return
+	}
+
+	Responser(c, 201, "User with esuccessfully created")
+}
--- models/models.go
+++ models/models.go
@@ -1,1 +1,53 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// User (collection)
+
+// {
+// 	"_id"	: "ObjectId()"
+// 	"email"	: "example@example.com",
+// 	"phone_number"	: "08238374928",
+// 	"password"	: "hashedpassword",
+// 	"profile_picture"	: "http://example.com",
+// 	"contact": [
+// 		{
+// 			"_id"	: "ObjectId()"
+// 			"phone_number"	: "08238374928",
+// 			"profile_picture"	: "http://example.com",
+// 		},
+// 		{
+// 			"_id"	: "ObjectId()"
+// 			"phone_number"	: "08238374928",
+// 			"profile_picture"	: null,
+// 		},
+// 	]
+// }
+
+type Contact struct {
+	PhoneNumber    string `bson:"phone_number"`
+	ProfilePicture string `bson:"profile_picture"`
+}
+
+type UserSetting struct {
+	OnlineStatus       string `bson:"online_status"`
+	ProfilePictureSeen string `bson:"profile_picture_seen"`
+	ChatByOther        string `bson:"chat_by_other"`
+}
+type User struct {
+	ID             primitive.ObjectID `bson:"_id"`
+	CreatedAt      time.Time          `bson:"created_at"`
+	LastOnline     time.Time          `bson:"last_online"`
+	UserName       string             `bson:"user_name"`
+	Bio            string             `bson:"bio"`
+	Email          string             `bson:"email"`
+	PhoneNumber    string             `bson:"phone_number"`
+	Password       string             `bson:"password"`
+	ProfilePicture string             `bson:"profile_picture"`
+	Contact        []Contact          `bson:"contact"`
+	UserSetting    UserSetting        `bson:"user_setting"`
+}
