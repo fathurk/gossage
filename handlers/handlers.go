c20139123fba48e709938f43fa9a655aeb330f6a
fathurkaryim <fathurkaryim@gmail.com>
2023-06-04 14:26:41 +0700

    fix: add data for responser

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -13,15 +13,26 @@ var (
 )
 
 type GenericResponse struct {
-	Code    int    `json:"code"`
-	Status  string `json:"status"`
-	Message string `json:"message"`
+	Code    int         `json:"code"`
+	Status  string      `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
 
-func Responser(c *gin.Context, Code int, Message string) {
+func Responser(c *gin.Context, Code int, Message string, Data interface{}) {
+	if Data == nil {
+		c.JSON(Code, &GenericResponse{
+			Code:    Code,
+			Status:  http.StatusText(Code),
+			Message: Message,
+			Data:    nil,
+		})
+		return
+	}
 	c.JSON(Code, &GenericResponse{
 		Code:    Code,
 		Status:  http.StatusText(Code),
 		Message: Message,
+		Data:    Data,
 	})
 }
